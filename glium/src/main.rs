dd9ffe99b0d7fc2107352642e8e00c0ac2a930d1
Brandon DeRosier <x@bdero.me>
2016-04-17 02:33:54 -0400

    Detect largest monitor

--- glium/src/main.rs
+++ glium/src/main.rs
@@ -1,9 +1,11 @@
 extern crate glium;
 
+use glium::glutin::MonitorId;
 
 struct Monitor {
-    id: glium::glutin::MonitorId,
+    total_pixels: u32,
     name: String,
+    id: glium::glutin::MonitorId,
 }
 
 impl Monitor {
@@ -11,9 +13,15 @@ impl Monitor {
         let dimensions = id.get_dimensions();
         let name = id.get_name().unwrap();
         Monitor {
+            total_pixels: dimensions.0*dimensions.1,
             id: id,
             name: format!("{} ({}x{})", name, dimensions.0, dimensions.1),
         }
+    }
+
+    fn get_total_pixels(&self) -> u32 {
+        let dimensions = self.id.get_dimensions();
+        dimensions.0*dimensions.1
     }
 }
 
@@ -27,7 +35,7 @@ fn main() {
     for (i, mon) in monitors.iter().enumerate() {
         println!("{}: {}", i, mon.name);
     }
-
+    let largest_monitor = monitors.iter().max_by_key(|x| x.get_total_pixels()).unwrap();
 
     let display = glium::glutin::WindowBuilder::new()
         .with_title(String::from("Glium test"))
